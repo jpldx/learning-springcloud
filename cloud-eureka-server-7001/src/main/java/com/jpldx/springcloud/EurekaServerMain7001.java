4ee3c96335498cf4259edaee986c7e36081dc66a
cxd <979309838@qq.com>
2021-05-11 13:33:35 +0800

    feat

--- cloud-eureka-server-7001/src/main/java/com/jpldx/springcloud/EurekaServerMain7001.java
+++ cloud-eureka-server-7001/src/main/java/com/jpldx/springcloud/EurekaServerMain7001.java
@@ -2,17 +2,17 @@ package com.jpldx.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author chen.xudong
  * @since 1.0.0
  */
-@EnableEurekaClient
+@EnableEurekaServer // 开启Eureka服务端
 @SpringBootApplication
-public class OrderMain80 {
+public class EurekaServerMain7001 {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class, args);
+        SpringApplication.run(EurekaServerMain7001.class, args);
     }
 }
--- cloud-provider-payment-8001/src/main/java/com/jpldx/springcloud/PaymentMain8001.java
+++ cloud-provider-payment-8001/src/main/java/com/jpldx/springcloud/PaymentMain8001.java
@@ -2,11 +2,13 @@ package com.jpldx.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author chen.xudong
  * @since 1.0.0
  */
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"com.jpldx.springcloud"})
 public class PaymentMain8001 {
 
